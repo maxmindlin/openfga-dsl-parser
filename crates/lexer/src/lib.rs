d8b938c0cdb04c90cd1311a7e8457b95d62afb69
Ben Elbert <benjamin.elbert@oliveai.com>
2022-07-25 16:59:09 -0400

    feat: Add lexer for generating a token stream for DSL

---
+++ crates/lexer/src/lib.rs
@@ -0,0 +1,48 @@
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+pub enum Token {
+    // Tokens can be literal strings, of any length.
+
+    #[token("type")]
+    Type,
+
+    #[token("relations")]
+    Relations,
+
+    #[token("define")]
+    Define,
+
+    #[token("as")]
+    As,
+
+    #[token("self")]
+    This,
+
+    #[token("or")]
+    Or,
+
+    #[token("and")]
+    And,
+
+    #[token("but not")]
+    Difference,
+
+    #[regex(r"[a-z_]+\sfrom\s[a-z_]+", priority = 2)]
+    From,
+
+    // // Or regular expressions.
+    // #[regex("[a-z_]+", priority = 1)]
+    // Text,
+
+    // #[token(" ")]
+    // Space,
+
+    // Logos requires one token variant to handle errors,
+    // it can be named anything you wish.
+    #[error]
+    // We can also use this variant to define whitespace,
+    // or any other matches we wish to skip.
+    #[regex(r"[\s\t\n]+", logos::skip)]
+    IsThisTheERROR,
+}
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use lexer::{Token};
+use logos::Logos;
+
+fn main() {
+    let mut lex = Token::lexer("type define viewer from owner\n");
+
+    println!("{:?}", lex.next());
+    println!("{:?}", lex.span());
+    println!("{:?}", lex.slice());
+
+    loop {
+        let next = lex.next();
+        if next == None {
+            break;
+        }
+        println!("{:?}", &next);
+        println!("{:?}", lex.span());
+        println!("{:?}", lex.slice());
+    }
+
+
+    // assert_eq!(lex.next(), Some(Token::Define));
+    // assert_eq!(lex.span(), 0..6);
+    // assert_eq!(lex.slice(), "define");
+
+    // assert_eq!(lex.next(), Some(Token::Space));
+    // assert_eq!(lex.span(), 6..7);
+    // assert_eq!(lex.slice(), " ");
+
+    // assert_eq!(lex.next(), Some(Token::From));
+    // assert_eq!(lex.span(), 7..25);
+    // assert_eq!(lex.slice(), "viewer from owner");
+
+    // assert_eq!(lex.next(), None);
+}
