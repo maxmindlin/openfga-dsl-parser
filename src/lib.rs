ead6953a31c87b9e663a054f89b3ca6c013d1deb
Max Mindlin <maxmindlin@gmail.com>
2022-07-27 22:36:26 -0400

    e2e json example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+pub use lexer::*;
+pub use parser::*;
+pub use json;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn e2e_json_serialize() {
+        let i = "type organization
+  relations
+    define member as self
+type document
+  relations
+    define owner as self
+    define can_share as owner or editor or owner from parent";
+
+        let lex = Lexer::new(i);
+        let mut parser = Parser::new(lex);
+        let doc = parser.parse_document().unwrap();
+
+        let jsont = json::JsonTransformer::new(&doc);
+        let json = jsont.serialize();
+        println!("{json}");
     }
 }
