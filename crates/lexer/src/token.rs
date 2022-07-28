9034de8e2fda0805ab4f11e71e218efd3e8e241e
Max Mindlin <maxmindlin@gmail.com>
2022-07-27 20:16:17 -0400

    working lexer

---
+++ crates/lexer/src/token.rs
@@ -0,0 +1,58 @@
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum TokenKind {
+    // keywords
+    Type,
+    Relations,
+    Define,
+    As,
+    This,
+    Or,
+    And,
+    From,
+
+    Text,
+
+    Newline,
+    EOF,
+
+    Illegal,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Token {
+    lit: String,
+    kind: TokenKind,
+    // span: Span,
+}
+
+// #[derive(Debug, PartialEq, Eq, Clone)]
+// pub struct Span {
+//     start: usize,
+//     end: usize,
+// }
+
+impl Token {
+    pub fn new(lit: String, kind: TokenKind) -> Self {
+        Self {
+            lit,
+            kind,
+            // span: Span { start, end },
+        }
+    }
+}
+
+impl TokenKind {
+    pub fn is_to_keyword(literal: &str) -> Option<Self> {
+        match literal {
+            "type" => Some(Self::Type),
+            "relations" => Some(Self::Relations),
+            "define" => Some(Self::Define),
+            "as" => Some(Self::As),
+            "self" => Some(Self::This),
+            "or" => Some(Self::Or),
+            "and" => Some(Self::And),
+            "from" => Some(Self::From),
+            _ => None,
+        }
+    }
+}
